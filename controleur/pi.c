6345b9450a682b612530c4f2fe27ae99404cf104
Horacio Sanchez <horaciosanchez.bora2014@gmail.com>
2024-04-20 12:15:59 +0200

    Communication par wifi

--- controleur/pi.c
+++ controleur/pi.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <wiringPi.h>
-#include <wiringSerial.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
 #define PIN_BLEU 1
 #define PIN_ROUGE 4
@@ -21,48 +24,81 @@ int main() {
     pinMode(PIN_JAUNE, INPUT);
     pinMode(PIN_VERT, INPUT);
 
-    char port[20];
-    int serial_port = -1;
-    char choix[20];
+    // Configuration de l'adresse IP et du point d'accès Wi-Fi
+    const char *wifi_interface = "wlan0";
+    const char *wifi_ssid = "MonPointDacces";
+    const char *wifi_password = "MotDePasse";
 
-    printf("Entrez le nom du port série de votre adaptateur WiFi USB : ");
-    scanf("%s", choix);
+    // Configuration de l'adresse IP et du port
+    const char *server_ip = "192.168.4.1"; // Adresse IP du serveur Wi-Fi
+    int server_port = 8888; // Port à utiliser
 
-    serial_port = serialOpen(choix, 9600);
-    if (serial_port < 0) {
-        fprintf(stderr, "Impossible d'ouvrir le port série %s.\n", choix);
+    // Configurer l'interface Wi-Fi
+    system("sudo ip link set dev wlan0 down");
+    system("sudo ip addr flush dev wlan0");
+    system("sudo ip link set dev wlan0 up");
+    system("sudo ip addr add 192.168.4.1/24 broadcast 192.168.4.255 dev wlan0");
+
+    // Lancement du service DHCP
+    system("sudo service dnsmasq restart");
+
+    // Lancement du point d'accès
+    system("sudo hostapd -B /etc/hostapd/hostapd.conf");
+
+    // Création de la socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Erreur lors de la création de la socket");
         return 1;
     }
 
-    printf("Port série %s ouvert avec succès.\n", choix);
+    // Configuration de l'adresse du serveur
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
 
-    while(1) {
-        if (analogRead(PIN_BLEU) > SEUIL) {
-            serialPuts(serial_port, "B\n");
-            while (analogRead(PIN_BLEU) > SEUIL) {
-                delay(DELAI);
-            }
+    // Attente de connexion des clients
+    int client_sockfd;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    while (1) {
+        client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_sockfd < 0) {
+            perror("Erreur lors de l'acceptation de la connexion client");
+            continue;
         }
-        if (analogRead(PIN_ROUGE) > SEUIL) {
-            serialPuts(serial_port, "R\n");
-            while (analogRead(PIN_ROUGE) > SEUIL) {
-                delay(DELAI);
+
+        while (1) {
+            if (analogRead(PIN_BLEU) > SEUIL) {
+                send(client_sockfd, "B\n", 2, 0);
+                while (analogRead(PIN_BLEU) > SEUIL) {
+                    delay(DELAI);
+                }
             }
-        }
-        if (analogRead(PIN_VERT) > SEUIL) {
-            serialPuts(serial_port, "G\n");
-            while (analogRead(PIN_VERT) > SEUIL) {
-                delay(DELAI);
+            if (analogRead(PIN_ROUGE) > SEUIL) {
+                send(client_sockfd, "R\n", 2, 0);
+                while (analogRead(PIN_ROUGE) > SEUIL) {
+                    delay(DELAI);
+                }
             }
-        }
-        if (analogRead(PIN_JAUNE) > SEUIL) {
-            serialPuts(serial_port, "Y\n");
-            while (analogRead(PIN_JAUNE) > SEUIL) {
-                delay(DELAI);
+            if (analogRead(PIN_VERT) > SEUIL) {
+                send(client_sockfd, "G\n", 2, 0);
+                while (analogRead(PIN_VERT) > SEUIL) {
+                    delay(DELAI);
+                }
             }
+            if (analogRead(PIN_JAUNE) > SEUIL) {
+                send(client_sockfd, "Y\n", 2, 0);
+                while (analogRead(PIN_JAUNE) > SEUIL) {
+                    delay(DELAI);
+                }
+            }
+            delay(DELAI);
         }
-        delay(DELAI);
     }
 
+    close(sockfd);
     return 0;
 }
